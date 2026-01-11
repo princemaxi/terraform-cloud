83edad2dd067ec62065b2151583cb9de258b4194
princemaxi <princemaxwellugochukwu@gmail.com>
2026-01-11 21:01:28 +0100

    migrated state to terraform cloud

--- backend.tf
+++ backend.tf
@@ -1,9 +1,21 @@
+#terraform {
+#   backend "s3" {
+#     bucket         = "princemaxi-dev-terraform-bucket"
+#     key            = "global/terraform/terraform.tfstate"
+#     region         = "eu-west-2"
+#     dynamodb_table = "terraform-locks"
+#     encrypt        = true
+#   }
+#}
+
+
 terraform {
-   backend "s3" {
-     bucket         = "princemaxi-dev-terraform-bucket"
-     key            = "global/terraform/terraform.tfstate"
-     region         = "eu-west-2"
-     dynamodb_table = "terraform-locks"
-     encrypt        = true
-   }
+  backend "remote" {
+    organization = "DevOps-Palace"
+
+    workspaces {
+      name = "terraform-cloud"
+    }
+  }
 }
+
